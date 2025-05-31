112c8d968c239e36dc11a58a83e5e8aef56a531e
mariia.pidlisna <mariia.pidlisna@gmail.com>
2025-05-31 23:42:35 +0300

    add hero

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,11 +1,28 @@
-const openBtnEl = document.querySelector('[data-action="open"]');
-const closeBtnEl = document.querySelector('[data-action="close"]');
-const burgerMenuEl = document.querySelector('[data-visible]');
+// const openBtnEl = document.querySelector('[data-action="open"]');
+// const closeBtnEl = document.querySelector('[data-action="close"]');
+// const burgerMenuEl = document.querySelector('[data-visible]');
 
-openBtnEl.addEventListener('click', e => {
-  burgerMenuEl.dataset.visible = 'open';
-});
+// openBtnEl.addEventListener('click', e => {
+//   burgerMenuEl.dataset.visible = 'open';
+// });
 
-closeBtnEl.addEventListener('click', e => {
-  burgerMenuEl.dataset.visible = 'close';
+// closeBtnEl.addEventListener('click', e => {
+//   burgerMenuEl.dataset.visible = 'close';
+// });
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const openBtnEl = document.querySelector('[data-action="open"]');
+  const closeBtnEl = document.querySelector('[data-action="close"]');
+  const burgerMenuEl = document.querySelector('[data-visible]');
+
+  openBtnEl.addEventListener("click", () => {
+    burgerMenuEl.dataset.visible = "open";
+  });
+
+  closeBtnEl.addEventListener("click", () => {
+    burgerMenuEl.dataset.visible = "close";
+  });
 });
