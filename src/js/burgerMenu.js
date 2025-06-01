5611ed9083fb19c3fcc796e962db40b86b2755a9
mariia.pidlisna <mariia.pidlisna@gmail.com>
2025-06-01 09:42:26 +0300

    fix burgerMenu hero.js

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,28 +1,13 @@
-// const openBtnEl = document.querySelector('[data-action="open"]');
-// const closeBtnEl = document.querySelector('[data-action="close"]');
-// const burgerMenuEl = document.querySelector('[data-visible]');
+const openBtnEl = document.querySelector('[data-action="open"]');
+const closeBtnEl = document.querySelector('[data-action="close"]');
+const burgerMenuEl = document.querySelector('[data-visible]');
 
-// openBtnEl.addEventListener('click', e => {
-//   burgerMenuEl.dataset.visible = 'open';
-// });
-
-// closeBtnEl.addEventListener('click', e => {
-//   burgerMenuEl.dataset.visible = 'close';
-// });
-
-
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  const openBtnEl = document.querySelector('[data-action="open"]');
-  const closeBtnEl = document.querySelector('[data-action="close"]');
-  const burgerMenuEl = document.querySelector('[data-visible]');
-
-  openBtnEl.addEventListener("click", () => {
-    burgerMenuEl.dataset.visible = "open";
-  });
-
-  closeBtnEl.addEventListener("click", () => {
-    burgerMenuEl.dataset.visible = "close";
-  });
+openBtnEl.addEventListener('click', e => {
+  burgerMenuEl.dataset.visible = 'open';
 });
+
+closeBtnEl.addEventListener('click', e => {
+  burgerMenuEl.dataset.visible = 'close';
+});
+
+
