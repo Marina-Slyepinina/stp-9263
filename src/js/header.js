b4f4d11f45ce56798a84c55e794026771a92b144
Marina <marina.slepinina3@gmail.com>
2025-06-01 17:54:05 +0300

    fix base.css, header and smth else

--- src/js/header.js
+++ src/js/header.js
@@ -1,15 +1,26 @@
 const openBtnEl = document.querySelector('[data-action="open"]');
-const closeBtnEl = document.querySelector('[data-action="close"]');
+const closeBtns = document.querySelectorAll('[data-action="close"]');
 const burgerMenuEl = document.querySelector('#burger-menu');
 const navLinks = document.querySelectorAll(".nav-link");
 const sections = document.querySelectorAll("section");
 
 openBtnEl.addEventListener("click", () => {
   burgerMenuEl.dataset.visible = "open";
+  openBtnEl.classList.add("hidden");
 });
 
-closeBtnEl.addEventListener("click", () => {
-  burgerMenuEl.dataset.visible = "close";
+closeBtns.forEach(btn =>
+  btn.addEventListener("click", () => {
+    burgerMenuEl.dataset.visible = "close";
+    openBtnEl.classList.remove("hidden");
+  })
+);
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    burgerMenuEl.dataset.visible = "close";
+    openBtnEl.classList.remove("hidden");
+  });
 });
 
 // Активна лінка під час прокрутки
