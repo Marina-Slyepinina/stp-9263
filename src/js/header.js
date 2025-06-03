489fc33fa504709a95ded8cb9e394a90b16a37bf
Marina <marina.slepinina3@gmail.com>
2025-06-03 22:03:38 +0300

    fix header position and style

--- src/js/header.js
+++ src/js/header.js
@@ -18,25 +18,9 @@ closeBtns.forEach(btn =>
 
 navLinks.forEach(link => {
   link.addEventListener("click", () => {
+    navLinks.forEach(l => l.classList.remove("active"));
+    link.classList.add("active");
     burgerMenuEl.dataset.visible = "close";
     openBtnEl.classList.remove("hidden");
-  });
-});
-
-// Активна лінка під час прокрутки
-window.addEventListener("scroll", () => {
-  let current = "";
-  sections.forEach((section) => {
-    const sectionTop = section.offsetTop - 80;
-    if (scrollY >= sectionTop) {
-      current = section.getAttribute("id");
-    }
-  });
-
-  navLinks.forEach((link) => {
-    link.classList.remove("active");
-    if (link.getAttribute("href") === "#" + current) {
-      link.classList.add("active");
-    }
   });
 });
